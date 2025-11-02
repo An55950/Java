6cecf22b3b303fea9960b9bf7475498ab758da9f
An55950 <ankitdhankar74@gmail.com>
2025-11-02 19:19:47 +0530

    Create Student.java

---
+++ JAVA EXP 8/Part a: Dependency Injection in Spring Using Java-Based Configuration/main/java/Student.java
@@ -0,0 +1,15 @@
+package com.example.springdi;
+
+public class Student {
+    private Course course;
+
+    // Constructor Injection
+    public Student(Course course) {
+        this.course = course;
+    }
+
+    public void displayInfo() {
+        System.out.println("Student details:");
+        course.showCourse();
+    }
+}
